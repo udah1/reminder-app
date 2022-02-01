28cc0bf2028373bb2b25e106b9476e4736c435fa
Huri, Yehuda <Yehuda.Huri2@uscellular.com>
2022-02-01 18:53:12 +0200

    package.json remove redundents

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import  'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.scss';
+import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
